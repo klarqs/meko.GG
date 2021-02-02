88bd7b306e4800ca2dfc7b935a19f9efaafd9b6d
paulklart <paulkolawole24@yahoo.com>
2021-02-02 06:15:32 -0500

    checkedout

---
+++ web/src/main/java/com/meko/MekoPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.meko;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MekoPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MekoPlatformApplication.class, args);
+	}
+
+}
---
+++ web/src/test/java/com/meko/MekoPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.meko;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MekoPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
